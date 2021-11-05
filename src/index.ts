328af0131f04c1ceababb33b10fe1e47979d2c97
lianjianjie <jjlian@iflytek.com>
2021-11-05 09:33:28 +0800

    引入amd模块

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,43 @@
 import "kity"
-import "../kityminderCore/kityminder"
+import "../kityminderCore/expose-kityminder"
+import "./style.css"
 
 console.log("kityminder-core 加载成功了")
 
 window.onload = evt => {
   // @ts-ignore
-  new window.kityminder.Minder({
+  const km = new window.kityminder.Minder({
     renderTo: "body",
   })
+
+  const data = {
+    root: {
+      data: {
+        id: "1",
+        text: "中心主题",
+        dir: "right",
+        layout: null,
+      },
+      children: [
+        {
+          id: "1-1",
+          text: "分支主题",
+          dir: "right",
+          layout: null,
+        },
+
+        {
+          id: "1-2",
+          text: "分支主题",
+          dir: "right",
+          layout: null,
+        },
+      ],
+    },
+    template: "default",
+    version: "1.4.43",
+  } as any
+
+  km.importData("json", JSON.stringify(data))
+  // km.importJson(data)
 }
